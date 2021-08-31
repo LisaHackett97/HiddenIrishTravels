e5aa120b1fef3e38e25f9e120cc17cc20cd395e3
LisaHackett97 <lisa-hackett@hotmail.com>
2021-08-31 07:40:21 +0000

    change base framework to materialize. Add nav

---
+++ static/css/js/script.js
@@ -0,0 +1,13 @@
+/*
+jquery for materialize
+*/
+
+$(document).ready(function () {
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+});
+
+// $(document).ready(function () {
+//     $(".dropdown-trigger").dropdown({hover: false});
+//});
+
