f39651c6de5cd5f8b0f977ca1297e5d9f9c482c1
LisaHackett97 <lisa-hackett@hotmail.com>
2021-09-01 14:43:50 +0000

    Add jquery function for password check

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,14 @@ $(document).ready(function () {
     });
     $(".collapsible").collapsible();
 });
+
+/* code for password confirm from this post
+https://stackoverflow.com/questions/21727317/how-to-check-confirm-password-field-in-form-without-reloading-page
+*/
+
+$('#password, #confirm_password').on('keyup', function () {
+    if ($('#password').val() == $('#confirm_password').val()) {
+      $('#message').html('Passwords Match. Click register to continue').css('color', '#172A3A');
+    } else 
+      $('#message').html('Passwords do not Match').css('color', 'red');
+  });
