9702e47ec48b15d813c7bc3510368698c531e46c
LisaHackett97 <lisa-hackett@hotmail.com>
2021-09-01 06:45:33 +0000

    Add basic registration html and render template

--- app.py
+++ app.py
@@ -27,6 +27,12 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
+
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
