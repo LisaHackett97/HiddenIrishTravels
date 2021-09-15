440c0a1af0aae0025b8157465d1ccbcb080ffc28
LisaHackett97 <lisa-hackett@hotmail.com>
2021-09-15 09:07:29 +0000

    Change font size on flash mages for visibility

--- app.py
+++ app.py
@@ -104,15 +104,15 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(request.form.get("username")))
                     return redirect(url_for(
                         "user_page", username=session['user']))
             else:
                 # invald password
-                flash("incorrect Username and/password")
+                flash("Incorrect Username and/password")
                 return redirect(url_for('login'))
         else:
-            flash("incorrect Username and/password")
+            flash("Incorrect Username and/password")
             return redirect(url_for('login'))
     return render_template("login.html")
 
